3fddcf9cad48ad0c7d1a70c25353a16b09dbcc21
AlshaheerSoft2050 <shahir4soft@gmail.com>
2024-08-29 00:29:47 +0300

    with_omra_setting

--- my_app/overrides/customer.py
+++ my_app/overrides/customer.py
@@ -8,18 +8,18 @@ class CustomCustomer(Customer):
         now = getdate()
         
         # التحقق من عدد أرقام جواز السفر
-        if len(self.custom_passport_number) != 6:
-            frappe.throw(_("يجب أن يكون رقم الجواز مكونًا من 6 أرقام بالضبط."))
+        # if len(self.custom_passport_number) != 8:
+         #    frappe.throw(_("يجب أن يكون رقم الجواز مكونًا من 6 أرقام بالضبط."))
         
         # التحقق من تاريخ التأشيرة
-        visa_date = getdate(self.custom_visa_date)
-        if visa_date >= now:
-            frappe.throw(_("يجب أن يكون تاريخ التأشيرة أصغر من تاريخ اليوم."))
+        # visa_date = getdate(self.custom_visa_date)
+       #  if visa_date >= now:
+         #    frappe.throw(_("يجب أن يكون تاريخ التأشيرة أصغر من تاريخ اليوم."))
         
         # التحقق من تاريخ الدخول
         entry_date = getdate(self.custom_entry_date)
-        if entry_date > now:
-            frappe.throw(_("يجب أن يكون تاريخ الدخول  اليوم أو تاريخًا سابقًا."))
+        #if entry_date > now:
+        #     frappe.throw(_("يجب أن يكون تاريخ الدخول  اليوم أو تاريخًا سابقًا."))
         
         # تحويل عدد الأيام إلى عدد صحيح والتحقق منه
         days_stay = int(self.custom_number_of_days_of_stay)
@@ -40,26 +40,36 @@ class CustomCustomer(Customer):
 def update_remaining_days_for_all_customers():
     customers = frappe.db.get_list(
         "Customer",
-        fields=["name", "custom_remaining_days_of_stay", "custom_entry_date", "custom_number_of_days_of_stay", "customer_name"]
+        fields=["name", "custom_remaining_days_of_stay", "custom_entry_date", "custom_number_of_days_of_stay", "customer_name", "custom_passport_number"]
     )
+
+    settings = frappe.db.get_value("omra_setting", None, ["maximum_period", "customer_link", "another_field"], as_dict=True)
+    maximum_period = settings.get("maximum_period")
+    maximum_period = int(settings.get("maximum_period"))
+    customer_links= settings.get("customer_link")
     now = getdate()
     for customer in customers:
         entry_date = getdate(customer.custom_entry_date)
         days_stay = int(customer["custom_number_of_days_of_stay"])
         days_since_entry = date_diff(now, customer["custom_entry_date"])
+        name=customer["name"]
         # Calculate remaining days
         remaining_days = days_stay - days_since_entry
         remaining_days = max(remaining_days, 0)  # Ensure non-negative value
         frappe.db.set_value('Customer', customer["name"], 'custom_remaining_days_of_stay', remaining_days)
-        if 0 < remaining_days <= 2:
+        if 0 < remaining_days <= maximum_period:
             customer_name = customer["customer_name"]
+            custom_passport_number = customer["custom_passport_number"]
             remaining_days = customer["custom_remaining_days_of_stay"]
-            message = f"تنبيه: العميل {customer_name} لديه {remaining_days} يومًا فقط متبقية من إقامته."
-            
+            customer_link = customer_links+""+name
+
+            message = f"تنبيه: العميل <a href='{customer_link}'>{customer_name}</a> (رقم الجواز: {maximum_period}) لديه {remaining_days} يومًا فقط متبقية من إقامته."
+
             frappe.publish_realtime(
                 event='msgprint',
                 message=message,
                 user=frappe.session.user
             )
+
     frappe.db.commit()       
 
